51b1b85151b85f2c0a82abd2008d7b157ed6c120
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-12-06 16:09:34 +0900

    seed

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name="visard",
+    version="0.1",
+    author="Amir HajiBabaei T.",
+    author_email="a.hajibabaei.86@gmail.com",
+    description="A visualization helper tool",
+    url="https://github.com",
+    package_dir={'visard':'visard'},
+    packages=['visard'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

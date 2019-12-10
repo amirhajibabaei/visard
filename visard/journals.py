a6be1c320757263efa68bcfaab05dc0ad591b508
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-12-10 20:25:28 +0900

    journals

---
+++ visard/journals.py
@@ -0,0 +1,29 @@
+from visard.rc_settings import cm_to_inches as inches
+
+
+nature = {
+    'columns': 2,
+    'single': inches(8.9),
+    'double': inches(18.3),
+    'single-double': None,
+    'page': inches(24.7),
+}
+
+
+physical_review = {
+    'columns': 2,
+    'single': inches(8.6),
+    'double': inches(17.8),
+    'single-double': None,
+    'page': None,
+}
+physical_review_letters = physical_review_e = physical_review_x = physical_review
+
+
+science_advances = {
+    'columns': 2,
+    'single': 3.5,
+    'double': 7.3,
+    'single-double': 5.,
+    'page': None,
+}
